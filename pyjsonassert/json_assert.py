ec7bf045f9c3ea85988263836837c6043fbfe1f2
Javier Seixas <dev@javierseixas.com>
2018-06-11 21:16:41 -0400

    Fixes assertion problem with json in string type

--- pyjsonassert/json_assert.py
+++ pyjsonassert/json_assert.py
@@ -3,6 +3,13 @@ import json
 
 
 def assert_json(expected_json, current_json, allow_unexpected_fields=True, allow_missing_fields=False):
+
+    if isinstance(expected_json, str):
+        expected_json = json.loads(expected_json)
+
+    if isinstance(current_json, str):
+        current_json = json.loads(current_json)
+
     differences = json.loads(diff(expected_json, current_json, syntax='symmetric', dump=True))
 
     reserved_keynames = ["$delete", "$insert"]
--- tests/test_assertion.py
+++ tests/test_assertion.py
@@ -7,29 +7,39 @@ class TestJsonAssert(unittest.TestCase):
     json_2 = {"animal": "cat"}
     json_3 = {"animal": "dog", "object": "table"}
 
+    json_str_1 = "{\"animal\": \"dog\"}"
+    json_str_2 = "{\"animal\": \"cat\"}"
+    json_str_3 = "{\"animal\": \"dog\", \"object\": \"table\"}"
+
     def test_it_should_assert_when_jsons_are_equal(self):
 
         assert_json(self.json_1, self.json_1)
+        assert_json(self.json_str_1, self.json_str_1)
 
     def test_it_should_fail_when_jsons_have_same_key_and_different_value(self):
 
         self.assertRaises(Exception, assert_json, self.json_1, self.json_2)
+        self.assertRaises(Exception, assert_json, self.json_str_1, self.json_str_2)
 
     def test_it_should_assert_when_jsons_match_one_key_but_current_json_contains_another_key_value(self):
 
         assert_json(self.json_1, self.json_3)
+        assert_json(self.json_str_1, self.json_str_3)
 
     def test_it_should_fail_when_jsons_match_one_key_but_one_contains_another_key_value_and_not_new_value_are_accepted(self):
 
         self.assertRaises(Exception, assert_json, self.json_1, self.json_3, False)
+        self.assertRaises(Exception, assert_json, self.json_str_1, self.json_str_3, False)
 
     def test_it_should_fail_when_expected_json_has_more_keys_than_current_json(self):
 
         self.assertRaises(Exception, assert_json, self.json_3, self.json_1)
+        self.assertRaises(Exception, assert_json, self.json_str_3, self.json_str_1)
 
     def test_it_should_assert_when_expected_json_has_more_keys_than_current_json_and_it_is_allowed(self):
 
         assert_json(self.json_3, self.json_1, True, True)
+        assert_json(self.json_str_3, self.json_str_1, True, True)
 
 
 
